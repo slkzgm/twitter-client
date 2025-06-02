05a64eb51560ecf746e45c1ca1c4bb3600678668
slkzgm <contact@slkzgm.com>
2025-06-02 11:08:30 +0200

    update build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,21 +3,16 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'dist',
-  tsconfig: './tsconfig.build.json', // Use build-specific tsconfig
+  tsconfig: 'tsconfig.build.json',
   sourcemap: true,
   clean: true,
-  format: ['esm'], // Ensure you're targeting CommonJS
-  dts: true, // Skip DTS generation to avoid external import issues // Ensure you're targeting CommonJS
+  format: ['esm'], // Building ESM
+  dts: true,       // Generates index.d.ts
   external: [
-    'dotenv', // Externalize dotenv to prevent bundling
-    'fs', // Externalize fs to use Node.js built-in module
-    'path', // Externalize other built-ins if necessary
-    '@reflink/reflink',
-    '@node-llama-cpp',
+    'fs',
+    'path',
     'https',
     'http',
-    'agentkeepalive',
-    '@elizaos/core',
-    // Add other modules you want to externalize
+    'agentkeepalive'
   ],
 });
